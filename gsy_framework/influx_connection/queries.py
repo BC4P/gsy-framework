f1d0c319d3ff3b9a20a11142dd239d263d5c9802
Lukas Walk <walk@fh-aachen.de>
2022-11-07 16:14:06 +0100

    added smartmeter influx options; added berg pilot to bc4p setup

--- gsy_framework/influx_connection/queries.py
+++ gsy_framework/influx_connection/queries.py
@@ -1,4 +1,5 @@
 from gsy_framework.influx_connection.connection import InfluxConnection
+from gsy_framework.constants_limits import GlobalConfig
 
 class InfluxQuery:
     def __init__(self, influxConnection: InfluxConnection):
@@ -25,7 +26,8 @@ class RawQuery(InfluxQuery):
 
 
 class DataQuery(InfluxQuery):
-    def __init__(self, influxConnection: InfluxConnection):
+    def __init__(self, influxConnection: InfluxConnection, invert=False):
+        self.invert = invert
         super().__init__(influxConnection)
 
     def _process(self):
@@ -45,5 +47,25 @@ class DataQuery(InfluxQuery):
 
         # convert to dictionary
         df.set_index("index", inplace=True)
+
+        #invert data
+        if(self.invert == True):
+            df["mean"]  = df["mean"]  * -1
+
         ret = df.to_dict().get("mean")
         return ret
+
+class DataQueryMQTT(DataQuery):
+    def __init__(self, influxConnection: InfluxConnection,
+                        power_column: str,
+                        device: str,
+                        tablename: str,
+                        duration = GlobalConfig.sim_duration,
+                        start = GlobalConfig.start_date,
+                        interval = GlobalConfig.slot_length.in_minutes(),
+                        invert=False):
+        super().__init__(influxConnection, invert)
+
+        end = start + duration
+        qstring = f'SELECT mean("{power_column}") FROM "{tablename}" WHERE "device" =~ /^{device}$/ AND time >= \'{start.to_datetime_string()}\' AND time <= \'{end.to_datetime_string()}\' GROUP BY time({interval}m) fill(0)'
+        self.set(qstring)
