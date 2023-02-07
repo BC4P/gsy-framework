3bfe41145f9917bb24aec75029181c3767a4820a
Lukas Walk <walk@fh-aachen.de>
2023-02-07 16:41:45 +0100

    added eupon pilot query

--- gsy_framework/influx_connection/queries_eupen.py
+++ gsy_framework/influx_connection/queries_eupen.py
@@ -12,11 +12,19 @@ class DataQueryEupen(InfluxQuery):
                         duration = GlobalConfig.sim_duration,
                         start = GlobalConfig.start_date,
                         interval = GlobalConfig.slot_length.in_minutes()):
+        self.power_column = power_column
+        self.tablename = tablename
+        self.key = key
+        self.location = location
         super().__init__(influxConnection)
-
+        
+    def query_string(self,
+                duration = duration(days=1),
+                start = GlobalConfig.start_date,
+                interval = GlobalConfig.slot_length.in_minutes(),
+                ):
         end = start + duration
-        qstring = f'SELECT mean("{power_column}") FROM "{tablename}" WHERE ("Location" = \'{location}\' AND "Key" = \'{key}\') AND time >= \'{start.to_datetime_string()}\' AND time <= \'{end.to_datetime_string()}\' GROUP BY time({interval}m), "Meter" fill(0)'    
-        self.set(qstring)
+        return f'SELECT mean("{power_column}") FROM "{tablename}" WHERE ("Location" = \'{location}\' AND "Key" = \'{key}\') AND time >= \'{start.to_datetime_string()}\' AND time <= \'{end.to_datetime_string()}\' GROUP BY time({interval}m), "Meter" fill(0)'
 
     def _process(self):
         # sum smartmeters
